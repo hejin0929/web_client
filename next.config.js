0787df65f2d64796344f9b7aa7a4fc700b634c29
huanghejin <>
2023-05-17 22:48:59 +0800

    feat: config less mudole

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withLess = require("@zeit/next-less");
+
+const nextConfig = withLess({
   reactStrictMode: true,
   serverRuntimeConfig: {},
-};
+  // 添加Less配置选项（可选）
+  lessLoaderOptions: {
+    /* 在这里可以添加Less loader的配置 */
+    // 示例：启用CSS Modules
+    modules: true,
+  },
+});
 
 module.exports = nextConfig;
